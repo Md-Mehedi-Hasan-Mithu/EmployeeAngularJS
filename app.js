5ecba3efde783feac225f05dad4916e1c27c9d7b
Mehedi Hasan <mdmehedi3799@gmail.com>
2026-03-12 13:08:52 +0600

    Add salary form and submit buttons
    Add a new salary-form.html containing an employee selector, designation and salary inputs, and Save/Clear actions (ng-submit='save()', Clear resets the salary model). Also update employee-form.html to add a form-submit section with Save and Clear buttons (Clear wired to ng-click='clear()') to provide basic save/clear UI for forms.

--- app.js
+++ app.js
@@ -1,67 +1,73 @@
-var app = angular.module('employeeApp',['ngRoute']);
+var app = angular.module('employeeApp', ['ngRoute']);
 
-app.config(function($routeProvider){
-    $routeProvider
-     .when('/employee-form',{
-        templateUrl:'pages/employee-form.html',
-        controller:'EmployeeFormController'
-     })
-     .when('/salary-form',{
-        templateUrl:'pages/salary-form.html',
-        controller:'salaryFormController'
-     })
-     .when('/employee-list',{
-        templateUrl:'pages/employee-list',
-        controller:'employeeListController'
-     })
-     .otherwise({
-        redirectTo:'/employee-form'
-     });
+app.config(function($routeProvider) {
+  $routeProvider
+    .when('/employee-form', {
+      templateUrl: 'pages/employee-form.html',
+      controller: 'EmployeeFormController'
+    })
+    .when('/salary-form', {
+      templateUrl: 'pages/salary-form.html',
+      controller: 'SalaryFormController'
+    })
+    .when('/employee-list', {
+      templateUrl: 'pages/employee-list.html',
+      controller: 'EmployeeListController'
+    })
+    .otherwise({
+      redirectTo: '/employee-form'
+    });
 });
 
-//Employee Form Controller
-app.controller('EmployeeFormController', function($scope,$http){
-    $scope.employee = {};
+// Employee Form Controller
+app.controller('EmployeeFormController', function($scope, $http) {
+  $scope.employee = {};
 
-    $scope.divisions = ['Dhaka','Chittagong','Rajshahi','Khulna','Sylhet','Barishal','Rangpur','Mymensingh'];
+  $scope.divisions = ['Dhaka','Chittagong','Rajshahi',
+                      'Khulna','Sylhet','Barisal',
+                      'Rangpur','Mymensingh'];
 
-    $scope.districts = ['Dhaka','Gazipur','Narayanganj',
+  $scope.districts = ['Dhaka','Gazipur','Narayanganj',
                       'Chittagong','Cox\'s Bazar','Sylhet'];
 
-    $scope.religions = ['Islam','Hinduism','Christianity',
+  $scope.religions = ['Islam','Hinduism','Christianity',
                       'Buddhism','Others'];
 
-    $scope.save = function(){
-        $http.post('http://localhost:5000/api/employee', $scope.employee).then(function(){
-            alert('Employee saved Successfully!');
-            $scope.employee = {};
-        }, function(){
-            alert('Error saving employee!');
-        });
-    };
-    $scope.clear = function(){
+  $scope.save = function() {
+    $http.post('http://localhost:5000/api/employee', $scope.employee)
+      .then(function() {
+        alert('Employee saved successfully!');
         $scope.employee = {};
-    };
+      }, function() {
+        alert('Error saving employee!');
+      });
+  };
+
+  $scope.clear = function() {
+    $scope.employee = {};
+  };
 });
 
-app.controller('SalaryFormController',function($scope,$http){
-    $scope.salary = {};
-    $scope.employees = [];
+// Salary Form Controller
+app.controller('SalaryFormController', function($scope, $http) {
+  $scope.salary = {};
+  $scope.employees = [];
 
-    $http.get('http://localhost:5000/api/employee')
+  $http.get('http://localhost:5000/api/employee')
     .then(function(response) {
       $scope.employees = response.data;
     });
 
-    $scope.save = function() {
+  $scope.save = function() {
     $http.post('http://localhost:5000/api/salary', $scope.salary)
       .then(function() {
         alert('Salary saved successfully!');
         $scope.salary = {};
       });
   };
+});
 
-  // Employee List Controller
+// Employee List Controller
 app.controller('EmployeeListController', function($scope, $http) {
   $scope.employees = [];
   $scope.filter = {};
@@ -79,4 +85,3 @@ app.controller('EmployeeListController', function($scope, $http) {
     $scope.selectedEmployee = angular.copy(emp);
   };
 });
-})
